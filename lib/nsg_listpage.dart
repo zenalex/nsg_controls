25036fe5f880f9a1667a106f7222153b1c71bce2
zenalex <zenkov@nsgsoft.ru>
2022-05-23 21:44:18 +0300

    bugfix createNewItem

--- lib/nsg_listpage.dart
+++ lib/nsg_listpage.dart
@@ -164,7 +164,7 @@ class NsgListPage extends StatelessWidget {
       /// Новый объект
       icon2: Icons.add,
       onPressed2: () {
-        controller.currentItem = controller.createNewItem();
+        controller.createNewItem();
         Get.toNamed(elementEditPage);
       },
 
