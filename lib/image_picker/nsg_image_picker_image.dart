77ef3154e6e966c05642fa736c94db2c4c23192d
Andreus <andy.protech@gmail.com>
2022-07-22 17:47:56 +0300

    picker

---
+++ lib/image_picker/nsg_image_picker_image.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class NsgImagePickerImage {
+  Image image;
+  String description = '';
+  NsgImagePickerImage({required this.image, required this.description});
+}
--- lib/nsg_text.dart
+++ lib/nsg_text.dart
@@ -5,41 +5,33 @@ import 'package:nsg_controls/nsg_control_options.dart';
 class NsgTextType {
   TextStyle style;
   NsgTextType(this.style);
-  static NsgTextType h1 = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeXL, fontWeight: FontWeight.w500));
-  static NsgTextType h2 = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeL, fontWeight: FontWeight.w500));
-  static NsgTextType h3 = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeM, fontWeight: FontWeight.w500));
-  static NsgTextType text = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeM, fontWeight: FontWeight.normal));
-  static NsgTextType textS = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeS, fontWeight: FontWeight.normal));
-  static NsgTextType textXS = NsgTextType(TextStyle(
-      fontSize: ControlOptions.instance.sizeXS, fontWeight: FontWeight.normal));
+  static NsgTextType h1 = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeXL, fontWeight: FontWeight.w500));
+  static NsgTextType h2 = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeL, fontWeight: FontWeight.w500));
+  static NsgTextType h3 = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeM, fontWeight: FontWeight.w500));
+  static NsgTextType text = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeM, fontWeight: FontWeight.normal));
+  static NsgTextType textS = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeS, fontWeight: FontWeight.normal));
+  static NsgTextType textXS = NsgTextType(TextStyle(fontSize: ControlOptions.instance.sizeXS, fontWeight: FontWeight.normal));
 }
 
 class NsgTextStyle {
   TextStyle style;
   NsgTextStyle(this.style);
-  static NsgTextStyle normal =
-      NsgTextStyle(const TextStyle(fontWeight: FontWeight.normal));
-  static NsgTextStyle semibold =
-      NsgTextStyle(const TextStyle(fontWeight: FontWeight.w500));
-  static NsgTextStyle bold =
-      NsgTextStyle(const TextStyle(fontWeight: FontWeight.w600));
-  static NsgTextStyle extrabold =
-      NsgTextStyle(const TextStyle(fontWeight: FontWeight.w800));
+  static NsgTextStyle normal = NsgTextStyle(const TextStyle(fontWeight: FontWeight.normal));
+  static NsgTextStyle semibold = NsgTextStyle(const TextStyle(fontWeight: FontWeight.w500));
+  static NsgTextStyle bold = NsgTextStyle(const TextStyle(fontWeight: FontWeight.w600));
+  static NsgTextStyle extrabold = NsgTextStyle(const TextStyle(fontWeight: FontWeight.w800));
 }
 
 class NsgText extends StatelessWidget {
+  final EdgeInsets margin;
+  final int? maxLines;
+  final TextOverflow? overflow;
   final Color? color;
   final Color? backColor;
   final String text;
   final NsgTextType? type;
   final NsgTextStyle? style;
-  const NsgText(this.text,
-      {Key? key, this.color, this.backColor, this.type, this.style})
+  const NsgText(this.text, {Key? key, this.margin = EdgeInsets.zero, this.overflow, this.maxLines, this.color, this.backColor, this.type, this.style})
       : super(key: key);
 
   @override
@@ -51,11 +43,17 @@ class NsgText extends StatelessWidget {
     if (style != null) mergedStyle = mergedStyle.merge(style!.style);
     mergedStyle = mergedStyle.merge(TextStyle(color: color));
     return backColor == null
-        ? Text(
-            text,
-            style: mergedStyle,
+        ? Padding(
+            padding: margin,
+            child: Text(
+              text,
+              style: mergedStyle,
+              maxLines: maxLines,
+              overflow: overflow,
+            ),
           )
         : Container(
+            margin: margin,
             padding: const EdgeInsets.all(2),
             decoration: BoxDecoration(color: backColor),
             child: Text(
