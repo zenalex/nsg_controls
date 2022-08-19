fc5771419da9157d1be332db0aad133621c72e7a
Andreus <andy.protech@gmail.com>
2022-08-19 15:52:26 +0300

    снекбар

---
+++ lib/widgets/nsg_snackbar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../nsg_control_options.dart';
+
+enum NsgSnarkBarType {
+  info('Информация', Icons.info_outline),
+  warning('Предупреждение', Icons.warning_outlined),
+  error('Ошибка', Icons.error_outline);
+
+  final String title;
+  final IconData icon;
+
+  const NsgSnarkBarType(this.title, this.icon);
+}
+
+void nsgSnackbar({String? title, required String text, NsgSnarkBarType? type, Duration? duration}) {
+  Get.snackbar(title ?? (type == null ? '' : type.title), text,
+      icon: type == null ? null : Icon(type.icon),
+      duration: duration ?? const Duration(seconds: 3),
+      maxWidth: 300,
+      snackPosition: SnackPosition.BOTTOM,
+      barBlur: 0,
+      overlayBlur: 0,
+      colorText: ControlOptions.instance.colorMainText,
+      backgroundColor: ControlOptions.instance.colorMainDark);
+}
