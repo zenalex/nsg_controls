3ab682b6f656f04d68f3723f8876a69a11ca603b
AndyProtech <andy.protech@gmail.com>
2022-09-01 16:53:54 +0300

    виджет ошибки и метод ошибки для аппбара

---
+++ lib/widgets/nsg_errorpage.dart
@@ -0,0 +1,37 @@
+// импорт
+import 'package:flutter/material.dart';
+
+import '../nsg_text.dart';
+
+class NsgErrorPage extends StatelessWidget {
+  final String? text;
+  const NsgErrorPage({Key? key, this.text = ''}) : super(key: key);
+
+  title() {
+    List<String> list = (text ?? 'Неизвестная ошибка|||').split('|||');
+    return list[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> list = (text ?? 'Неизвестная ошибка|||').split('|||');
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: NsgText(
+                "Ошибка ${list[0]}",
+                type: NsgTextType.h1,
+              ),
+            ),
+            if (list.length > 1) Flexible(child: SingleChildScrollView(child: NsgText(list[1]))),
+          ],
+        ),
+      ),
+    );
+  }
+}
