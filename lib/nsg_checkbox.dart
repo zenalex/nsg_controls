29b99c73e5aa5325fe2f32ed10e93c72ab1818e6
AndyProtech <andy.protech@gmail.com>
2022-09-19 12:07:27 +0300

    чекбокс

--- lib/nsg_checkbox.dart
+++ lib/nsg_checkbox.dart
@@ -9,6 +9,7 @@ class NsgCheckBox extends StatefulWidget {
   final bool? radio;
   final bool value;
   final double? height;
+  final double? width;
   final Function(bool currentValue) onPressed;
   final EdgeInsets margin;
   final bool toggleInside;
@@ -27,6 +28,7 @@ class NsgCheckBox extends StatefulWidget {
       this.radio,
       required this.value,
       this.height = 44,
+      this.width,
       required this.onPressed,
       this.margin = const EdgeInsets.fromLTRB(0, 0, 0, 0),
       this.simple = false})
@@ -49,57 +51,66 @@ class _NsgCheckBoxState extends State<NsgCheckBox> {
   Widget build(BuildContext context) {
     //if (!widget.toggleInside) boxValue = widget.value;
 
-    return Material(
-      color: Colors.transparent,
-      child: Padding(
-        padding: widget.margin,
-        child: Stack(
-          alignment: Alignment.bottomLeft,
-          children: [
-            if (widget.validateText != '' && widget.simple != true)
-              Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Text(
-                    widget.validateText,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 10, color: ControlOptions.instance.colorError),
-                  )),
-            InkWell(
-              onTap: () {
-                _boxValue = !_boxValue;
-                widget.onPressed(_boxValue);
-                if (widget.toggleInside) setState(() {});
-              },
-              hoverColor: ControlOptions.instance.colorMain.withOpacity(0.1),
-              splashColor: ControlOptions.instance.colorMain.withOpacity(0.2),
-              //focusColor: ControlOptions.instance.colorMain.withOpacity(0.5),
-              highlightColor: ControlOptions.instance.colorMain.withOpacity(0.2),
-              child: SizedBox(
-                height: widget.height,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (widget.radio == true)
-                      Icon(_boxValue == true ? Icons.radio_button_checked : Icons.radio_button_unchecked_outlined,
-                          color: _boxValue == true ? ControlOptions.instance.colorMainDark : ControlOptions.instance.colorMainDark)
-                    else
-                      Icon(_boxValue == true ? Icons.check_box_outlined : Icons.check_box_outline_blank,
-                          color: _boxValue == true ? ControlOptions.instance.colorMainDark : ControlOptions.instance.colorMainDark),
-                    const SizedBox(width: 4),
-                    Expanded(
-                      child: Text(
-                        widget.label,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(height: 1, color: ControlOptions.instance.colorText, fontSize: ControlOptions.instance.sizeM),
-                      ),
-                    )
-                  ],
+    return Container(
+      width: widget.width ?? double.infinity,
+      child: Material(
+        color: Colors.transparent,
+        child: Padding(
+          padding: widget.margin,
+          child: Stack(
+            alignment: Alignment.bottomLeft,
+            children: [
+              if (widget.validateText != '' && widget.simple != true)
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      widget.validateText,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 10, color: ControlOptions.instance.colorError),
+                    )),
+              InkWell(
+                onTap: () {
+                  _boxValue = !_boxValue;
+                  widget.onPressed(_boxValue);
+                  if (widget.toggleInside) setState(() {});
+                },
+                hoverColor: ControlOptions.instance.colorMain.withOpacity(0.1),
+                splashColor: ControlOptions.instance.colorMain.withOpacity(0.2),
+                //focusColor: ControlOptions.instance.colorMain.withOpacity(0.5),
+                highlightColor: ControlOptions.instance.colorMain.withOpacity(0.2),
+                child: SizedBox(
+                  height: widget.height,
+                  child: Row(
+                    children: [
+                      if (widget.radio == true)
+                        Icon(_boxValue == true ? Icons.radio_button_checked : Icons.radio_button_unchecked_outlined,
+                            color: _boxValue == true
+                                ? ControlOptions.instance.colorMainDark
+                                : ControlOptions.instance.colorMainDark)
+                      else
+                        Icon(_boxValue == true ? Icons.check_box_outlined : Icons.check_box_outline_blank,
+                            color: _boxValue == true
+                                ? ControlOptions.instance.colorMainDark
+                                : ControlOptions.instance.colorMainDark),
+                      const SizedBox(width: 4),
+                      Flexible(
+                        child: Text(
+                          widget.label,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              height: 1,
+                              color: ControlOptions.instance.colorText,
+                              fontSize: ControlOptions.instance.sizeM),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
