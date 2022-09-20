602e60b93696136ff70a00f558f0520a29c4fb4a
zenalex <zenkov@nsgsoft.ru>
2022-09-20 20:16:45 +0300

    fix

--- lib/nsg_input.dart
+++ lib/nsg_input.dart
@@ -454,8 +454,7 @@ class _NsgInputState extends State<NsgInput> {
           widget.dataItem.setFieldValue(widget.fieldName, item);
           if (widget.onChanged != null) widget.onChanged!(widget.dataItem);
           if (widget.onEditingComplete != null) widget.onEditingComplete!(widget.dataItem, widget.fieldName);
-          //TODO: эксперимент. Нужен ли здесь setState?
-          //setState(() {});
+          setState(() {});
         };
         Get.toNamed(widget.selectionForm);
       }
