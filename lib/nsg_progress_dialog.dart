dcf3627b01528e7d77359b206bc4673cf18db8bd
AndyProtech <andy.protech@gmail.com>
2023-01-19 18:17:12 +0300

    delayed progress

--- lib/nsg_progress_dialog.dart
+++ lib/nsg_progress_dialog.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nsg_controls/nsg_controls.dart';
@@ -20,37 +19,9 @@ class NsgProgressDialog {
     // открываем popup с прогрессбаром NsgProgressBar
     //print("SHOW");
     Get.dialog(
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-          child: Center(
-            child: Container(
-              width: 250,
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              decoration: BoxDecoration(color: const Color.fromARGB(255, 255, 255, 255), borderRadius: BorderRadius.circular(15)),
-              child: Material(
-                color: Colors.transparent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(textDialog, textAlign: TextAlign.center, style: TextStyle(color: ControlOptions.instance.colorText)),
-                    const SizedBox(height: 10),
-                    NsgProgressBar(text: text),
-                    if (canStopped == true)
-                      NsgButton(
-                        text: 'Отмена',
-                        onPressed: () {
-                          if (requestStop != null) requestStop!();
-                          cancelToken?.calcel();
-                          Get.back();
-                        },
-                      ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
+        NsgProgressDialogWidget(
+            canStopped: canStopped, cancelToken: cancelToken, percent: percent, requestStop: requestStop, text: text, textDialog: textDialog, visible: visible),
+        barrierColor: Colors.transparent,
         barrierDismissible: false);
   }
 
@@ -61,4 +32,80 @@ class NsgProgressDialog {
     }
   }
   // При нажатии на кнопку отмены вызываем requestStop - убираем кнопку отмены, пишем "обработка отмены"
+}
+
+class NsgProgressDialogWidget extends StatefulWidget {
+  final String? text;
+  final double? percent;
+  final bool? canStopped;
+  final Function? requestStop;
+  final String textDialog;
+  final NsgCancelToken? cancelToken;
+  final bool visible;
+  NsgProgressDialogWidget(
+      {super.key,
+      required this.text,
+      required this.percent,
+      required this.canStopped,
+      required this.requestStop,
+      required this.textDialog,
+      required this.cancelToken,
+      required this.visible});
+
+  @override
+  State<NsgProgressDialogWidget> createState() => _NsgProgressDialogWidgetState();
+}
+
+class _NsgProgressDialogWidgetState extends State<NsgProgressDialogWidget> {
+  bool loadingTooLong = false;
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        loadingTooLong = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !loadingTooLong
+        ? const SizedBox()
+        : BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.black26),
+              child: Center(
+                child: Container(
+                  width: 250,
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  decoration: BoxDecoration(color: const Color.fromARGB(255, 255, 255, 255), borderRadius: BorderRadius.circular(15)),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(widget.textDialog, textAlign: TextAlign.center, style: TextStyle(color: ControlOptions.instance.colorText)),
+                        const SizedBox(height: 10),
+                        NsgProgressBar(text: widget.text!),
+                        if (widget.canStopped == true)
+                          NsgButton(
+                            text: 'Отмена',
+                            onPressed: () {
+                              if (widget.requestStop != null) widget.requestStop!();
+                              widget.cancelToken?.calcel();
+                              Get.back();
+                            },
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
 }
