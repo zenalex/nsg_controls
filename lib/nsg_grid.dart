8285f68ac084e3a5d72c84b4f83d3276fe0be9f3
AndyProtech <andy.protech@gmail.com>
2023-02-09 14:07:18 +0300

    grid fix

--- lib/nsg_grid.dart
+++ lib/nsg_grid.dart
@@ -7,26 +7,29 @@ class NsgGrid extends StatelessWidget {
 
   /// Количество виджетов по горизонтали
   final int crossAxisCount;
-  const NsgGrid({Key? key, required this.children, this.crossAxisCount = 3})
-      : super(key: key);
+  const NsgGrid({Key? key, required this.children, this.crossAxisCount = 3}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     int count = 0;
+    int rowCount = 0;
     List<Widget> list = [];
     List<Widget> row = [];
     for (var element in children) {
-      row.add(Expanded(flex: 2, child: element));
+      row.add(Expanded(flex: 1, child: element));
       count++;
+      rowCount++;
       if (count > crossAxisCount - 1) {
         list.add(Row(children: row));
         count = 0;
+        rowCount = 0;
         row = [];
       }
     }
     if (row.isNotEmpty) {
-      row.add(const Expanded(child: SizedBox()));
-      row.insert(0, const Expanded(child: SizedBox()));
+      for (var i = 0; i < crossAxisCount - rowCount; i++) {
+        row.add(const Expanded(child: SizedBox()));
+      }
       list.add(Row(children: row));
     }
 
