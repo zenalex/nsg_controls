b33001521e0ddfd84546bd4d62bbbe7c5ffb847f
AndyProtech <andy.protech@gmail.com>
2023-04-07 17:02:54 +0300

    tabs

---
+++ lib/tabs/nsg_tabs_tab.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NsgTabsTab {
+  Widget tab;
+  Widget tabSelected;
+  Widget child;
+  NsgTabsTab({required this.tab, required this.tabSelected, required this.child});
+}
