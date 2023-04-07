1b96c16a8456862549056ba5c74ac1fb283f695c
AndyProtech <andy.protech@gmail.com>
2023-04-07 21:09:42 +0300

    grid gaps

--- lib/nsg_grid.dart
+++ lib/nsg_grid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nsg_controls/nsg_border.dart';
 
 /// Виджет Grid без Aspect Ratio
 class NsgGrid extends StatelessWidget {
@@ -8,9 +9,13 @@ class NsgGrid extends StatelessWidget {
   /// Выравнивание по центру
   final bool centered;
 
+  /// Отступы между элементами сетки
+  final double vGap;
+  final double hGap;
+
   /// Количество виджетов по горизонтали
   final int crossAxisCount;
-  const NsgGrid({Key? key, required this.children, this.crossAxisCount = 3, this.centered = true}) : super(key: key);
+  const NsgGrid({Key? key, required this.children, this.crossAxisCount = 3, this.centered = true, this.vGap = 0, this.hGap = 0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,25 +25,38 @@ class NsgGrid extends StatelessWidget {
     List<Widget> row = [];
     for (var element in children) {
       row.add(Expanded(flex: 2, child: element));
+
       count++;
       rowCount++;
       if (count > crossAxisCount - 1) {
+        for (var i = 0; i <= row.length; i++) {
+          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+          i++;
+        }
         list.add(Row(children: row));
+
         count = 0;
         rowCount = 0;
         row = [];
       }
     }
     if (row.isNotEmpty) {
-/* --------------------------------------------------------- если выравнивание по левому краю -------------------------------------------------------- */
       var dif = crossAxisCount - rowCount;
       var difAdd = (dif / 2).floor();
+      /* ---------------------------------------------------------- Выравнивание по левому краю --------------------------------------------------------- */
       if (!centered) {
         for (var i = 0; i < dif; i++) {
           row.add(const Expanded(flex: 2, child: SizedBox()));
         }
+        for (var i = 0; i <= row.length; i++) {
+          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+          i++;
+        }
         list.add(Row(children: row));
-      } else {
+      } else
+
+      /* ------------------------------------------------------------ Выравнивание по центру ------------------------------------------------------------ */
+      {
         for (var i = 0; i < difAdd; i++) {
           row.insert(0, const Expanded(flex: 2, child: SizedBox()));
           row.add(const Expanded(flex: 2, child: SizedBox()));
@@ -47,10 +65,18 @@ class NsgGrid extends StatelessWidget {
           row.insert(0, const Expanded(flex: 1, child: SizedBox()));
           row.add(const Expanded(flex: 1, child: SizedBox()));
         }
+        for (var i = 0; i <= row.length; i++) {
+          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+          i++;
+        }
         list.add(Row(children: row));
       }
     }
 
+    for (var i = 0; i <= list.length; i++) {
+      if (i < list.length - 1) list.insert(i + 1, SizedBox(height: vGap));
+      i++;
+    }
     return Column(children: list);
   }
 }
