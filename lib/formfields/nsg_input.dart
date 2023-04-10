c2d566f0bf0db12099d2c31fe17294c04b2d93d3
anshot12 <antoniofl@mail.ru>
2023-04-10 15:30:41 +0300

    fix

--- lib/formfields/nsg_input.dart
+++ lib/formfields/nsg_input.dart
@@ -671,33 +671,34 @@ class _NsgInputState extends State<NsgInput> {
         NsgNavigator.instance.toPage(widget.selectionForm);
       }
     } else if (inputType == NsgInputType.dateValue) {
-      NsgTimePicker(
-        initialTime: Duration(hours: DateTime.now().hour, minutes: DateTime.now().minute),
-        onClose: (Duration endDate) {},
-      ).showPopup(context, widget.dataItem[widget.fieldName].hour, widget.dataItem[widget.fieldName].minute, (value) {
-        if (widget.onChanged != null) widget.onChanged!(widget.dataItem);
-        if (widget.onEditingComplete != null) {
-          widget.onEditingComplete!(widget.dataItem, widget.fieldName);
-        }
-        widget.dataItem[widget.fieldName] = value;
-        setState(() {});
-      });
-      // NsgDatePicker(
-      //         firstDateTime: widget.firstDateTime,
-      //         lastDateTime: widget.lastDateTime,
-      //         initialTime: DateTime(01, 01, 01).isAtSameMomentAs(widget.dataItem[widget.fieldName]) ||
-      //                 DateTime(1754, 01, 01).isAtSameMomentAs(widget.dataItem[widget.fieldName])
-      //             ? widget.initialDateTime ?? DateTime.now()
-      //             : widget.dataItem[widget.fieldName],
-      //         onClose: (value) {})
-      //     .showPopup(context, widget.dataItem[widget.fieldName], (value) {
-      //   if (widget.onChanged != null) widget.onChanged!(widget.dataItem);
-      //   if (widget.onEditingComplete != null) {
-      //     widget.onEditingComplete!(widget.dataItem, widget.fieldName);
-      //   }
-      //   widget.dataItem[widget.fieldName] = value;
-      //   setState(() {});
-      // });
+      widget.formatDateTime == 'HH:mm'
+          ? NsgTimePicker(
+              initialTime: Duration(hours: DateTime.now().hour, minutes: DateTime.now().minute),
+              onClose: (Duration endDate) {},
+            ).showPopup(context, widget.dataItem[widget.fieldName].hour, widget.dataItem[widget.fieldName].minute, (value) {
+              if (widget.onChanged != null) widget.onChanged!(widget.dataItem);
+              if (widget.onEditingComplete != null) {
+                widget.onEditingComplete!(widget.dataItem, widget.fieldName);
+              }
+              widget.dataItem[widget.fieldName] = value;
+              setState(() {});
+            })
+          : NsgDatePicker(
+                  firstDateTime: widget.firstDateTime,
+                  lastDateTime: widget.lastDateTime,
+                  initialTime: DateTime(01, 01, 01).isAtSameMomentAs(widget.dataItem[widget.fieldName]) ||
+                          DateTime(1754, 01, 01).isAtSameMomentAs(widget.dataItem[widget.fieldName])
+                      ? widget.initialDateTime ?? DateTime.now()
+                      : widget.dataItem[widget.fieldName],
+                  onClose: (value) {})
+              .showPopup(context, widget.dataItem[widget.fieldName], (value) {
+              if (widget.onChanged != null) widget.onChanged!(widget.dataItem);
+              if (widget.onEditingComplete != null) {
+                widget.onEditingComplete!(widget.dataItem, widget.fieldName);
+              }
+              widget.dataItem[widget.fieldName] = value;
+              setState(() {});
+            });
     }
   }
 
