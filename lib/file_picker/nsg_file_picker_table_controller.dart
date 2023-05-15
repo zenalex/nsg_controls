594cf501164955a3fcd06d5d57d2d996570d5f03
pro5 <pro5@SRV-FRZ-WEB>
2023-05-15 17:22:05 +0300

    newTableLogic, docState

--- lib/file_picker/nsg_file_picker_table_controller.dart
+++ lib/file_picker/nsg_file_picker_table_controller.dart
@@ -41,9 +41,9 @@ class NsgFilePickerTableController<T extends NsgDataItem> extends NsgDataTableCo
     try {
       for (var img in files) {
         ids.add(img.id);
-        if (img.isNew && img.id.isNotEmpty && (img.fileContent != null ||  img.filePath.isNotEmpty)) {
+        if (img.isNew && img.id.isNotEmpty && (img.fileContent != null || img.filePath.isNotEmpty)) {
           //TODO: убрать imageFile
-          File? imageFile = kIsWeb|| img.filePath.isEmpty ? null : File(img.filePath);
+          File? imageFile = kIsWeb || img.filePath.isEmpty ? null : File(img.filePath);
 
           var pic = await fileObjectToDataItem(img, imageFile);
           if (!table.rows.any((e) => e.id == pic.id)) {
@@ -55,7 +55,11 @@ class NsgFilePickerTableController<T extends NsgDataItem> extends NsgDataTableCo
       //TODO: УДАЛЕНИЕ ФАЙЛОВ!!!!!
       var itemsToDelete = table.rows.where((se) => !ids.contains(se.id)).toList();
       for (var row in itemsToDelete) {
-        table.rows.remove(row);
+        if (row.newTableLogic) {
+          row.docState = NsgDataItemDocState.deleted;
+        } else {
+          table.rows.remove(row);
+        }
       }
       progress.hide();
       // Get.back();
