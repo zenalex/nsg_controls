8739c6111e40a25503cbef1a1338141fe97762bd
AndyProtech <andy.protech@gmail.com>
2023-06-27 13:34:04 +0300

    fix dialog и snackbar

--- lib/widgets/nsg_snackbar.dart
+++ lib/widgets/nsg_snackbar.dart
@@ -24,6 +24,14 @@ void nsgSnackbar(
     TextAlign? textAlign}) {
   Flushbar(
     onTap: onTap,
+    boxShadows: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 15,
+        offset: Offset(0, -5),
+      )
+    ],
     backgroundColor: ControlOptions.instance.colorMain,
     messageColor: ControlOptions.instance.colorMainText,
     titleText: title == null
