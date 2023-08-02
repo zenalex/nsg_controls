1e57af124483e543a7a6fa68a036292d4f395595
AndyProtech <andy.protech@gmail.com>
2023-08-02 13:12:04 +0300

    grid fix

--- lib/dialog/show_nsg_dialog.dart
+++ lib/dialog/show_nsg_dialog.dart
@@ -2,9 +2,11 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:nsg_controls/nsg_border.dart';
 import 'package:nsg_controls/nsg_control_options.dart';
 
 import '../nsg_button.dart';
+import '../nsg_grid.dart';
 
 Future<dynamic> showNsgDialog(
     {required BuildContext context,
@@ -20,82 +22,84 @@ Future<dynamic> showNsgDialog(
       builder: (context) {
         return BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-          child: Container(
-            decoration: BoxDecoration(color: nsgtheme.colorModalBack),
-            child: SimpleDialog(
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              contentPadding: EdgeInsets.zero,
-              insetPadding: EdgeInsets.all(10),
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(color: nsgtheme.colorModalBack, borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 640),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                          alignment: Alignment.center,
+                          // width: double.infinity,
+                          decoration: BoxDecoration(color: ControlOptions.instance.colorMain),
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            title,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: ControlOptions.instance.colorMainText, fontSize: ControlOptions.instance.sizeL),
+                          )),
+                      Container(
+                        //  width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: ControlOptions.instance.colorMainBack,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                          child: child ??
+                              Text(
+                                text,
+                                textAlign: TextAlign.center,
+                              ),
+                        ),
+                      ),
+                      Container(
                         alignment: Alignment.center,
-                        width: double.infinity,
-                        decoration: BoxDecoration(color: ControlOptions.instance.colorMain),
-                        padding: const EdgeInsets.all(20),
-                        child: Text(
-                          title,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: ControlOptions.instance.colorMainText, fontSize: ControlOptions.instance.sizeL),
-                        )),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: ControlOptions.instance.colorMainBack,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-                        child: child ??
-                            Text(
-                              text,
-                              textAlign: TextAlign.center,
-                            ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      decoration: BoxDecoration(color: ControlOptions.instance.colorMainBack.withOpacity(0.9)),
-                      padding: const EdgeInsets.all(15),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: buttons ??
-                            [
-                              if (showCancelButton)
+                        decoration: BoxDecoration(color: ControlOptions.instance.colorMainBack.withOpacity(0.9)),
+                        padding: const EdgeInsets.all(15),
+                        child: NsgGrid(
+                          vGap: 10,
+                          hGap: 10,
+                          width: 300,
+                          children: buttons ??
+                              [
+                                if (showCancelButton)
+                                  NsgButton(
+                                    onPressed: () {
+                                      if (onCancel != null) {
+                                        onCancel();
+                                      } else {
+                                        Navigator.of(context).pop();
+                                      }
+                                    },
+                                    margin: const EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                                    width: 150,
+                                    height: 40,
+                                    text: 'Отмена',
+                                    color: Colors.black,
+                                    backColor: ControlOptions.instance.colorGrey,
+                                  ),
                                 NsgButton(
                                   onPressed: () {
-                                    if (onCancel != null) {
-                                      onCancel();
-                                    } else {
-                                      Navigator.of(context).pop();
-                                    }
+                                    Navigator.of(context).pop();
+                                    if (onConfirm != null) onConfirm();
                                   },
-                                  margin: const EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                                  margin: const EdgeInsets.symmetric(horizontal: 10),
                                   width: 150,
                                   height: 40,
-                                  text: 'Отмена',
-                                  color: Colors.black,
-                                  backColor: ControlOptions.instance.colorGrey,
+                                  text: 'Да',
                                 ),
-                              NsgButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                  if (onConfirm != null) onConfirm();
-                                },
-                                margin: const EdgeInsets.symmetric(horizontal: 10),
-                                width: 150,
-                                height: 40,
-                                text: 'Да',
-                              ),
-                            ],
-                      ),
-                    )
-                  ],
-                )
-              ],
+                              ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         );
--- lib/nsg_grid.dart
+++ lib/nsg_grid.dart
@@ -20,78 +20,81 @@ class NsgGrid extends StatelessWidget {
 
   /// Количество виджетов по горизонтали
   final int crossAxisCount;
-  NsgGrid({Key? key, required this.children, this.crossAxisCount = 3, this.centered = true, this.vGap = 0, this.hGap = 0, this.needExpanded = true, this.width})
+  const NsgGrid(
+      {Key? key, required this.children, this.crossAxisCount = 3, this.centered = true, this.vGap = 0, this.hGap = 0, this.needExpanded = true, this.width})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var axisCount = crossAxisCount;
-    if (width != null) {
-      axisCount = (Get.width / width!).floor();
-    }
-
-    int count = 0;
-    int rowCount = 0;
-    List<Widget> list = [];
-    List<Widget> row = [];
-    for (var element in children) {
-      if (needExpanded) {
-        row.add(Expanded(flex: 2, child: element));
-      } else {
-        row.add(element);
+    return LayoutBuilder(builder: (context, constraints) {
+      var axisCount = crossAxisCount;
+      if (width != null) {
+        axisCount = (constraints.maxWidth / width!).floor();
       }
 
-      count++;
-      rowCount++;
-      if (count > axisCount - 1) {
-        for (var i = 0; i <= row.length; i++) {
-          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
-          i++;
+      int count = 0;
+      int rowCount = 0;
+      List<Widget> list = [];
+      List<Widget> row = [];
+      for (var element in children) {
+        if (needExpanded) {
+          row.add(Expanded(flex: 2, child: element));
+        } else {
+          row.add(element);
         }
-        list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
 
-        count = 0;
-        rowCount = 0;
-        row = [];
+        count++;
+        rowCount++;
+        if (count > axisCount - 1) {
+          for (var i = 0; i <= row.length; i++) {
+            if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+            i++;
+          }
+          list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
+
+          count = 0;
+          rowCount = 0;
+          row = [];
+        }
       }
-    }
-    if (row.isNotEmpty) {
-      var dif = axisCount - rowCount;
-      var difAdd = (dif / 2).floor();
-      /* ---------------------------------------------------------- Выравнивание по левому краю --------------------------------------------------------- */
-      if (!centered) {
-        for (var i = 0; i < dif; i++) {
-          row.add(const Expanded(flex: 2, child: SizedBox()));
-        }
-        for (var i = 0; i <= row.length; i++) {
-          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
-          i++;
-        }
-        list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
-      } else
+      if (row.isNotEmpty) {
+        var dif = axisCount - rowCount;
+        var difAdd = (dif / 2).floor();
+        /* ---------------------------------------------------------- Выравнивание по левому краю --------------------------------------------------------- */
+        if (!centered) {
+          for (var i = 0; i < dif; i++) {
+            row.add(const Expanded(flex: 2, child: SizedBox()));
+          }
+          for (var i = 0; i <= row.length; i++) {
+            if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+            i++;
+          }
+          list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
+        } else
 
-      /* ------------------------------------------------------------ Выравнивание по центру ------------------------------------------------------------ */
-      {
-        for (var i = 0; i < difAdd; i++) {
-          row.insert(0, const Expanded(flex: 2, child: SizedBox()));
-          row.add(const Expanded(flex: 2, child: SizedBox()));
+        /* ------------------------------------------------------------ Выравнивание по центру ------------------------------------------------------------ */
+        {
+          for (var i = 0; i < difAdd; i++) {
+            row.insert(0, const Expanded(flex: 2, child: SizedBox()));
+            row.add(const Expanded(flex: 2, child: SizedBox()));
+          }
+          if (dif.isOdd) {
+            row.insert(0, const Expanded(flex: 1, child: SizedBox()));
+            row.add(const Expanded(flex: 1, child: SizedBox()));
+          }
+          for (var i = 0; i <= row.length; i++) {
+            if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
+            i++;
+          }
+          list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
         }
-        if (dif.isOdd) {
-          row.insert(0, const Expanded(flex: 1, child: SizedBox()));
-          row.add(const Expanded(flex: 1, child: SizedBox()));
-        }
-        for (var i = 0; i <= row.length; i++) {
-          if (i < row.length - 1) row.insert(i + 1, SizedBox(width: hGap));
-          i++;
-        }
-        list.add(IntrinsicHeight(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: row)));
       }
-    }
 
-    for (var i = 0; i <= list.length; i++) {
-      if (i < list.length - 1) list.insert(i + 1, SizedBox(height: vGap));
-      i++;
-    }
-    return Column(children: list);
+      for (var i = 0; i <= list.length; i++) {
+        if (i < list.length - 1) list.insert(i + 1, SizedBox(height: vGap));
+        i++;
+      }
+      return Column(children: list);
+    });
   }
 }
