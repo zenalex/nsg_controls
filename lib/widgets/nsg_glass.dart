9b546adde36f4e899457ad383dcb28c6e53fe97c
Mishasik <mishelzenkov@gmail.com>
2023-08-17 02:21:02 +0200

    Create nsg_glass.dart

---
+++ lib/widgets/nsg_glass.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+extension NsgGlassWidget<T extends Widget> on T {
+  ClipRRect asGlass({
+    double blurX = 10.0,
+    double blurY = 10.0,
+    Color tintColor = Colors.white,
+    bool frosted = true,
+    BorderRadius? clipBorderRadius = BorderRadius.zero,
+    Clip clipBehaviour = Clip.antiAlias,
+    TileMode tileMode = TileMode.clamp,
+    CustomClipper<RRect>? clipper,
+  }) {
+    return ClipRRect(
+      clipper: clipper,
+      clipBehavior: clipBehaviour,
+      borderRadius: clipBorderRadius,
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: blurX,
+          sigmaY: blurY,
+          tileMode: tileMode,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: (tintColor != Colors.transparent)
+                ? LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      tintColor.withOpacity(0.1),
+                      tintColor.withOpacity(0.08),
+                    ],
+                  )
+                : null,
+            image: frosted
+                ? const DecorationImage(
+                    image: AssetImage('images/noise.png', package: 'nsg_controls'),
+                    fit: BoxFit.cover,
+                  )
+                : null,
+          ),
+          child: this,
+        ),
+      ),
+    );
+  }
+}
