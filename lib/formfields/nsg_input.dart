a57f1c589530438320c784c9ea4e455d7820b301
AndyProtech <andy.protech@gmail.com>
2023-09-27 13:47:20 +0300

    валидация

--- lib/formfields/nsg_input.dart
+++ lib/formfields/nsg_input.dart
@@ -584,7 +584,7 @@ class _NsgInputState extends State<NsgInput> {
                     ),
                   ),
                 ),
-                if (widget.validateText != '')
+                if (getValidateText() != '')
                   Padding(
                     padding: const EdgeInsets.only(top: 2),
                     child: Text(
@@ -597,7 +597,7 @@ class _NsgInputState extends State<NsgInput> {
   }
 
   String getValidateText() {
-    if (widget.controller!.fieldsWithError.isNotEmpty) {
+    if (widget.controller != null && widget.controller!.fieldsWithError.isNotEmpty) {
       return widget.controller!.fieldsWithError[widget.fieldName]!;
     } else {
       return widget.validateText;
