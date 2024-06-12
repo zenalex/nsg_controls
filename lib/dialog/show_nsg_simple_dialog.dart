acc87f8fbba76badcf7702903167db2644b8824e
AndyProtech <andy.protech@gmail.com>
2024-06-12 14:47:53 +0300

    simpledialog

---
+++ lib/dialog/show_nsg_simple_dialog.dart
@@ -0,0 +1,47 @@
+// импорт
+
+import 'package:flutter/material.dart';
+import 'package:nsg_controls/nsg_controls.dart';
+
+Future<dynamic> showNsgSimpleDialog({
+  required BuildContext context,
+  // bool showCancelButton = true,
+  // String title = 'Необходимо подтверждение',
+  String text = 'Вы уверены?',
+  // String textConfirm = 'ОК',
+  // String textCancel = 'Отмена',
+  // Widget? child,
+  // List<Widget>? buttons,
+  // VoidCallback? onConfirm,
+  // VoidCallback? onCancel
+}) {
+  return showDialog(
+    barrierColor: nsgtheme.colorMainBack.withOpacity(0.9),
+    context: context,
+    builder: (context) {
+      return SimpleDialog(
+          insetPadding: const EdgeInsets.all(10),
+          titlePadding: EdgeInsets.zero,
+          contentPadding: const EdgeInsets.all(20),
+          backgroundColor: nsgtheme.colorModalBack,
+          children: [
+            Center(child: Text(text)),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  NsgButton(
+                    width: 100,
+                    text: 'ОК',
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            )
+          ]);
+    },
+  );
+}
