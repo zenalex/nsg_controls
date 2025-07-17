d60156465284032ae40e535338d2259e00958c0e
Mishasik <mishelzenkov@gmail.com>
2025-07-17 14:20:42 +0300

    Update search_widget.dart

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -8,14 +8,48 @@ class SearchWidget extends StatefulWidget {
   final VoidCallback? onSuffixIconTap;
   final NsgBaseController controller;
   final double borderRadius;
-  const SearchWidget({super.key, required this.controller, this.borderRadius = 100, this.onSuffixIconTap, this.suffixIcon});
+  final EdgeInsets? padding;
+  final TextEditingController? textController;
+  const SearchWidget({super.key, required this.controller, this.borderRadius = 100, this.onSuffixIconTap, this.suffixIcon, this.padding, this.textController});
 
   @override
-  State<SearchWidget> createState() => _SearchWidgetState();
+  State<SearchWidget> createState() => SearchWidgetState();
+
+  ///Для перекрытия в каждом проекте
+  InputDecoration decoration(SearchWidgetState state) => InputDecoration(
+    prefixIcon: Padding(
+      padding: const EdgeInsets.only(left: 12, right: 8),
+      child: Icon(NsgIcons.search, color: nsgtheme.colorTertiary, size: 20),
+    ),
+    enabledBorder: OutlineInputBorder(
+      gapPadding: 8,
+      borderSide: BorderSide(color: ControlOptions.instance.colorTertiary),
+      borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      gapPadding: 8,
+      borderSide: BorderSide(color: ControlOptions.instance.colorPrimary),
+      borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+    ),
+    suffixIcon: IconButton(
+      padding: const EdgeInsets.only(bottom: 0, right: 12, left: 8),
+      onPressed:
+          onSuffixIconTap ??
+          () {
+            state.textEditController.text = '';
+            controller.controllerFilter.searchString = state.textEditController.text;
+            controller.refreshData();
+          },
+      icon: suffixIcon ?? Icon(NsgIcons.close, color: nsgtheme.colorTertiary, size: 20),
+    ),
+    // prefixIcon: Icon(Icons.search),
+    hintText: tran.search,
+    hintStyle: TextStyle(color: ControlOptions.instance.colorTertiary, fontWeight: FontWeight.w500),
+  );
 }
 
-class _SearchWidgetState extends State<SearchWidget> {
-  TextEditingController textEditController = TextEditingController();
+class SearchWidgetState extends State<SearchWidget> {
+  late TextEditingController textEditController = widget.textController ?? TextEditingController();
 
   @override
   void initState() {
@@ -33,60 +67,38 @@ class _SearchWidgetState extends State<SearchWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 44,
-      child: TextField(
-        cursorColor: Theme.of(context).primaryColor,
-        controller: textEditController,
-        decoration: InputDecoration(
-          prefixIcon: Padding(
-            padding: const EdgeInsets.only(left: 12, right: 8),
-            child: Icon(NsgIcons.search, color: nsgtheme.colorTertiary, size: 20),
-          ),
-          enabledBorder: OutlineInputBorder(
-            gapPadding: 8,
-            borderSide: BorderSide(color: ControlOptions.instance.colorTertiary),
-            borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
-          ),
-          focusedBorder: OutlineInputBorder(
-            gapPadding: 8,
-            borderSide: BorderSide(color: ControlOptions.instance.colorPrimary),
-            borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
-          ),
-          suffixIcon: IconButton(
-            padding: const EdgeInsets.only(bottom: 0, right: 12, left: 8),
-            onPressed:
-                widget.onSuffixIconTap ??
-                () {
-                  textEditController.text = '';
-                  widget.controller.controllerFilter.searchString = textEditController.text;
-                  widget.controller.refreshData();
-                  setState(() {});
-                },
-            icon: widget.suffixIcon ?? Icon(NsgIcons.close, color: nsgtheme.colorTertiary, size: 20),
-          ),
-          // prefixIcon: Icon(Icons.search),
-          hintText: tran.search,
-          hintStyle: TextStyle(color: ControlOptions.instance.colorTertiary, fontWeight: FontWeight.w500),
-        ),
-        textAlignVertical: TextAlignVertical.bottom,
-        style: TextStyle(color: ControlOptions.instance.colorBase.c100),
-        onChanged: (val) {
-          widget.controller.top = 0;
-          widget.controller.controllerFilter.searchString = textEditController.text;
-          var filter = widget.controller.getRequestFilter;
-          if (textEditController.text.isNotEmpty) {
-            var params = <String, dynamic>{};
-            params.addAll({'globalSearch': true});
-            filter.params?.addAll(params);
+    return Padding(
+      padding: widget.padding ?? EdgeInsets.all(0),
+      child: SizedBox(
+        height: 44,
+        child: ListenableBuilder(
+          listenable: textEditController,
+          builder: (context, w) {
+            return TextField(
+              cursorColor: Theme.of(context).primaryColor,
+              controller: textEditController,
+              decoration: widget.decoration(this),
+              textAlignVertical: TextAlignVertical.bottom,
+              style: TextStyle(color: ControlOptions.instance.colorBase.c100),
+              onChanged: (val) {
+                widget.controller.top = 0;
+                widget.controller.controllerFilter.searchString = textEditController.text;
+                var filter = widget.controller.getRequestFilter;
+                if (textEditController.text.isNotEmpty) {
+                  var params = <String, dynamic>{};
+                  params.addAll({'globalSearch': true});
+                  filter.params?.addAll(params);
 
-            filter.params ??= params;
-          } else {
-            filter = widget.controller.getRequestFilter;
-          }
-          widget.controller.controllerFilter.refreshControllerWithDelay(filter: filter);
-          widget.controller.sendNotify();
-        },
+                  filter.params ??= params;
+                } else {
+                  filter = widget.controller.getRequestFilter;
+                }
+                widget.controller.controllerFilter.refreshControllerWithDelay(filter: filter);
+                widget.controller.sendNotify();
+              },
+            );
+          },
+        ),
       ),
     );
   }
