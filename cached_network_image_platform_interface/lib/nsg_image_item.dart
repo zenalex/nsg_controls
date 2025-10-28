b1fe74eb80a08ae90f2fb5e7955ea18ea0263a2c
Mishasik <mishelzenkov@gmail.com>
2025-10-28 19:12:41 +0300

    Update nsg_image_item.dart

--- cached_network_image_platform_interface/lib/nsg_image_item.dart
+++ cached_network_image_platform_interface/lib/nsg_image_item.dart
@@ -11,15 +11,4 @@ enum ImageSize {
   small,
   medium,
   large;
-
-  double get size {
-    switch (this) {
-      case small:
-        return 150;
-      case large:
-        return 1000;
-      case medium:
-        return 300;
-    }
-  }
 }
